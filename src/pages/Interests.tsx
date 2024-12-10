c80e8937f43984e02b02ae1f2d07f82b3485343a
jamzheng109 <jamzheng@bu.edu>
2024-12-10 03:37:03 -0500

    for deployment

--- src/pages/Interests.tsx
+++ src/pages/Interests.tsx
@@ -5,16 +5,17 @@ const Interests = () => {
   return (
     <section className="interests-section">
       <h2>My Interests</h2>
-      <ul className="interests-list">
-        <li>Web Development</li>
-        <li>Artificial Intelligence</li>
-        <li>Data Visualization</li>
-        <li>Gaming</li>
-        <li>Traveling</li>
-        <li>Photography</li>
-        <li>Machine Learning</li>
-        <li>Reading Tech Blogs</li>
-      </ul>
+        <ul className="interests-list">
+            <li>Web Development</li>
+            <li>Artificial Intelligence</li>
+            <li>Working out</li>
+            <li>Gaming</li>
+            <li>Traveling</li>
+            <li>Technology</li>
+            <li>Listening to Music</li>
+            <li>Pickle Ball</li>
+
+        </ul>
     </section>
   );
 };
